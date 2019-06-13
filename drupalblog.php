b5d087153593a229b2941686f9ab1153d9697b6e
Parth Grover <parth.grover@buildingblocks.in>
2019-06-13 14:32:25 +0530

    script update

--- drupalblog.php
+++ drupalblog.php
@@ -1,13 +1,24 @@
 <?php
-include('../../../inc/global.inc.php');
+
+$dbhost = 'localhost';
+$dbuser = 'root';
+$dbpass = '';
+$conn = mysqli_connect($dbhost, $dbuser, $dbpass);
+   
+if(! $conn ) {
+    die('Could not connect: ' . mysql_error());
+ }
+
+
 function blog_title():array
 {
+	global $conn;
 	$sql = "SELECT * FROM `drupal`.`node` WHERE `type` = 'blog'";
-    $res = ABDatabase::run_sql_query($sql);
+    $res = mysqli_query($conn,$sql);
     $userdata = array();
-    if (ABDatabase::num_rows($res) > 0)
+    if (mysqli_num_rows($res) > 0)
     {
-        while($result = ABDatabase::fetch_array($res)){
+        while($result = mysqli_fetch_assoc($res)){
             $userdata[] = $result;      
         }
     }
@@ -17,10 +28,11 @@ function blog_title():array
 
 function blog_body(...$nid):array
 {
+	global $conn;
 	$sql = "SELECT * FROM `drupal`.`field_data_body` WHERE `entity_id` = '".$nid[0]."'";
-    $res = ABDatabase::run_sql_query($sql);
-    if(ABDatabase::num_rows($res) > 0){
-        $bb_details = ABDatabase::fetch_array($res);
+    $res = mysqli_query($conn,$sql);
+    if(mysqli_num_rows($res) > 0){
+        $bb_details = mysqli_fetch_assoc($res);
         return $bb_details;
     } 
 	
@@ -29,22 +41,24 @@ function blog_body(...$nid):array
 
 function blog_image(...$nid)
 {
+	global $conn;
 	$sql ="SELECT * FROM `drupal`.`file_managed` fm INNER join `drupal`.`field_data_field_index_image` fi on fi.field_index_image_fid = fm.fid where `entity_id` = '".$nid[0]."'";
-	$res = ABDatabase::run_sql_query($sql);
-    if(ABDatabase::num_rows($res) > 0){
-        $bi_details = ABDatabase::fetch_array($res);
+	$res = mysqli_query($conn,$sql);
+    if(mysqli_num_rows($res) > 0){
+        $bi_details = mysqli_fetch_assoc($res);
         return $bi_details;
     } 
 }
 
 function blog_comment(...$nid)
 {
+	global $conn;
 	$sql ="SELECT * from `drupal`.`comment` WHERE `nid` = '".$nid[0]."'";
-	$res = ABDatabase::run_sql_query($sql);
+	$res = mysqli_query($conn,$sql);
    $userdata = array();
-    if (ABDatabase::num_rows($res) > 0)
+    if (mysqli_num_rows($res) > 0)
     {
-        while($result = ABDatabase::fetch_array($res)){
+        while($result = mysqli_fetch_assoc($res)){
             $userdata[] = $result;      
         }
     }
@@ -80,20 +94,20 @@ foreach($blogs as $blog):
 	}
 		
 	$post_insert_sql = "insert into `wordpress`.`wp_posts`(`post_date`,`post_date_gmt`,`post_content`,`post_title`,`post_status`,`comment_status`,`ping_status`,`post_name`,`post_type`) values('".$post_date."','".$post_date."','".$body_post."','".$post_title."','publish','open','open','".$post_name."','post')";
-	$post_insert_query = ABDatabase::run_sql_query($post_insert_sql);
+	$post_insert_query = mysqli_query($conn,$post_insert_sql);
 	if($post_insert_query)
-				$post_insert_id = ABDatabase::get_last_insert_id();
+				$post_insert_id =  mysqli_insert_id($conn);;
 					echo $post_insert_id.'<br>';
 			
 	$image_sql = "SELECT * from `wordpress`.`wp_posts` where post_title ='".$blog_image_path."'";
-	$res = ABDatabase::run_sql_query($image_sql);
-	$md_details = ABDatabase::fetch_array($res);
+	$res = mysqli_query($conn,$image_sql);
+	$md_details = mysqli_fetch_assoc($res);
 	$image_id = $md_details["ID"];
 	echo $image_id.'<br>';
 	
 	if(!empty($image_id)){
 	$image_thumb_sql = "insert into `wordpress`.`wp_postmeta`(`post_id`,`meta_key`,`meta_value`) values('".$post_insert_id."','_thumbnail_id','".$image_id."')";
-	$image_thumb_query = ABDatabase::run_sql_query($image_thumb_sql);
+	$image_thumb_query = mysqli_query($conn,$image_thumb_sql);
 	echo $image_thumb_sql.'<br>'; 
 	}
 	
@@ -105,7 +119,7 @@ foreach($blogs as $blog):
 		$comment_email = $comment["mail"];
 		
 		$comment_insert_sql = "insert into `wordpress`.`wp_comments`(`comment_post_ID`,`comment_author`,`comment_author_email`,`comment_date`,`comment_date_gmt`,`comment_content`,`comment_approved`) values('".$post_insert_id."','".$comment_author_name."','".$comment_email."','".$comment_date."','".$comment_date."','".$comment_subject."','1')";
-		$comment_insert_query = ABDatabase::run_sql_query($comment_insert_sql);
+		$comment_insert_query = mysqli_query($conn,$comment_insert_sql);
 		echo $comment_insert_sql.'<br>';
 		
 	}
